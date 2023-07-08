6a6001e0fe8b0d2e6a82bf4b87b8ce5d14202e6f
pablodru <pdrocha03@gmail.com>
2023-07-08 14:26:08 -0300

    correção na validação dos valores

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ const schemaLogin = joi.object({
 });
 
 const schemaTransation = joi.object({
-    value: joi.number().required(),
+    value: joi.number().precision(2).required(),
     description: joi.string().required(),
     type: joi.string().valid("in", "out").required()
 })
