3530f84d05fffa037a738763c23338e7e95863a8
pablodru <pdrocha03@gmail.com>
2023-07-09 19:19:38 -0300

    implementação de middlewares

--- src/routes/transactions.routes.js
+++ src/routes/transactions.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { getTransactions, newTransaction } from "../controllers/transactions.controller.js";
+import { validateSchema } from "../middlewares/validateSchema.js";
+import { schemaTransation } from "../schemas/transactions.schemas.js";
 
 const transactionRouter = Router();
 
-transactionRouter.post("/transation", newTransaction);
+transactionRouter.post("/transation",validateSchema(schemaTransation), newTransaction);
 transactionRouter.get("/transation", getTransactions);
 
 export default transactionRouter;
